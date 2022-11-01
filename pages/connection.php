e7cd83ac54ec7a2794e7961dbcaae2c631145c92
catherineumali <caumali46@gmail.com>
2022-11-02 01:19:02 +0800

    database

--- pages/connection.php
+++ pages/connection.php
@@ -1,4 +1,5 @@
-<?php		
-	// Establishing Connection with Server by passing inputs as a parameter
-	$con = mysqli_connect('localhost','root','','db_barangay') or die(mysqli_error());
-	date_default_timezone_set("Asia/Manila");
+<?php
+// Establishing Connection with Server by passing inputs as a parameter
+// $con = mysqli_connect('localhost', 'zhyiiynw_admin', '', 'zhyiiynw_db_barangay') or die(mysqli_error());
+$con = mysqli_connect('localhost', 'zhyiiynw_admin', 'p@$$w0rdYPP', 'zhyiiynw_db_barangay') or die(mysqli_error()); // -production
+date_default_timezone_set("Asia/Manila");
