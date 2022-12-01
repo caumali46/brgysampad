d03157ca1edee9e31f1f2a120fb5250afb856eea
catherineumali <caumali46@gmail.com>
2022-12-01 22:21:58 +0800

    images

--- main/bkup.php
+++ main/bkup.php
@@ -12,12 +12,28 @@
       <?php
       $squery = mysqli_query($con, "select * from tblactivity ");
       while ($row = mysqli_fetch_array($squery)) {
+
+
         echo '<div class="col-lg-4 mb-3">
                 <div class="box" data-aos="fade-up">
                   <span>' . $row['activity'] . '</span>
                   <p>' . $row['description'] . '</p>
+                </div>';
+
+        $p = mysqli_query($con, "SELECT * from tblactivityphoto where activityid = '" . $row['id'] . "' ");
+        while ($row1 = mysqli_fetch_array($p)) {
+          echo '<div style="
+                  margin-bottom:1em;
+                  width: 100%;
+                  height: 20em;
+                  background-size: cover;
+                  background-position: center;
+                  background-repeat: no-repeat;
+                  background-image: url(/pages/activity/photo/' . basename($row1['filename']) . '");
+                ">
                 </div>
               </div>';
+        }
       }
       ?>
     </div>
